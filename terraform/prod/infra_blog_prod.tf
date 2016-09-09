84bc33ee2c60d002062a99f102a95963c12f6fc2
clamorisse <bvcotero@gmail.com>
2016-09-08 20:53:58 -0400

    created global infrastructure

--- terraform/prod/infra_blog_prod.tf
+++ terraform/prod/infra_blog_prod.tf
@@ -40,46 +40,4 @@ EOF
     }
 }
 
-# CREATE USER TO DEPLOY BLOG
-
-module "iam-user" {
-  source = "github.com/clamorisse/modular-terraform-automation//modules/iam/users"
-
-  user_names = "${var.application-name}-user-${var.env}"
-}
-
-resource "aws_iam_user_policy" "blog-s3-deployment-policy" {
-    name = "${var.application-name}-policydeploy-${var.env}"
-    user = "${module.iam-user.users}"
-    policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:GetBucketLocation",
-        "s3:ListAllMyBuckets"
-      ],
-      "Resource": "arn:aws:s3:::*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:ListBucket"
-      ],
-      "Resource": "arn:aws:s3:::${var.bucket-name}"
-    },
-    {
-      "Action": [
-         "s3:PutObject",
-         "s3:GetObject"
-      ],
-      "Effect": "Allow",
-      "Resource": "arn:aws:s3:::${var.bucket-name}/*" 
-    }
-  ]
-}
-EOF
-}
 
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,14 @@
+# ---------------------------------------------------
+#               GLOBAL  VARIABLES
+# ---------------------------------------------------
+
+# AWS CONFIGURATION VARIABLES
+
+aws-region           = "us-east-1"
+profile-name         = "roman-bvc-admin"
+object-name          = "terraform.tfstate"
+application-name     = "blog-cotero" 
+env                  = "global"
+
+bucket-prod          = "blog.cotero.org"
+bucket-stage         = "blog-stage.cotero.org"
